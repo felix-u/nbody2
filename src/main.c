f5ade8fbf0f8d3fc7ac98ff8a143fc85ba43d887
Felix <29074327+felix-u@users.noreply.github.com>
2023-11-23 21:52:43 -0500

    show message box

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #define BASE_IMPLEMENTATION
 #include "base.h"
 
-int main(void) {
-    printf("Hello, world!\n");
+#include <windows.h>
+
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow
+) {
+    (void)hInstance;
+    (void)hPrevInstance;
+    (void)lpCmdLine;
+    (void)nCmdShow;
+
+    MessageBox(0, "This is nbody2", "nbody2", MB_OK | MB_ICONINFORMATION);
     return 0;
 }
