0c6e32f4ec39f42514460df5597f7a8a24ae871d
Felix <29074327+felix-u@users.noreply.github.com>
2023-11-25 01:03:06 -0500

    blit to buffer

--- src/main.c
+++ src/main.c
@@ -5,6 +5,53 @@
 
 static bool running;
 
+static BITMAPINFO bitmap_info;
+static void *bitmap_mem;
+static HBITMAP bitmap_handle;
+static HDC bitmap_device_ctx;
+
+static void resize_DIB_section(int width, int height) {
+    if (bitmap_handle) DeleteObject(bitmap_handle);
+    if (!bitmap_device_ctx) bitmap_device_ctx = CreateCompatibleDC(0);
+
+    bitmap_info = (BITMAPINFO){
+        .bmiHeader = { 
+            .biSize = sizeof(bitmap_info.bmiHeader),
+            .biWidth = width,
+            .biHeight = height,
+            .biPlanes = 1,
+            .biBitCount = 32,
+            .biCompression = BI_RGB,
+        },
+    };
+
+    bitmap_handle = CreateDIBSection(
+        bitmap_device_ctx,
+        &bitmap_info,
+        DIB_RGB_COLORS,
+        &bitmap_mem,
+        0, 0 
+    );
+}
+
+static void update_window(
+    HDC device_ctx, 
+    int x, 
+    int y, 
+    int width, 
+    int height
+) {
+    StretchDIBits(
+        device_ctx,
+        x, y, width, height,
+        x, y, width, height,
+        bitmap_mem,
+        &bitmap_info,
+        DIB_RGB_COLORS, 
+        SRCCOPY
+    );
+}
+
 LRESULT main_window_callback(
     HWND window_handle,
     UINT message,
@@ -39,11 +86,16 @@ LRESULT main_window_callback(
             int y = paint.rcPaint.top;
             int width = paint.rcPaint.right - paint.rcPaint.left;
             int height = paint.rcPaint.bottom - paint.rcPaint.top;
-            PatBlt(device_ctx, x, y, width, height, BLACKNESS);
+            update_window(device_ctx, x, y, width, height);
 
             EndPaint(window_handle, &paint);
         } break;
         case WM_SIZE: {
+            RECT client_rect;
+            GetClientRect(window_handle, &client_rect);
+            int width = client_rect.right - client_rect.left;
+            int height = client_rect.bottom - client_rect.top;
+            resize_DIB_section(width, height);
             OutputDebugStringA("WM_SIZE\n");
         } break;
         default: {
@@ -95,8 +147,10 @@ int CALLBACK WinMain(
         // TODO: handle error
     }
 
-    MSG message;
-    while (GetMessage(&message, 0, 0, 0) > 0) {
+    running = true;
+    while (running) {
+        MSG message;
+        if (GetMessage(&message, 0, 0, 0) <= 0) break;
         TranslateMessage(&message);
         DispatchMessage(&message);
     }
