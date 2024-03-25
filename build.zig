9a051bf5baffeb68c2fa4be8ec17206814003c5c
Felix <29074327+felix-u@users.noreply.github.com>
2024-03-24 22:25:19 -0400

    fix build

--- build.zig
+++ build.zig
@@ -1,20 +1,30 @@
+const builtin = @import("builtin");
 const std = @import("std");
-const raylib_build = @import("raylib/src/build.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimise = b.standardOptimizeOption(.{});
-
-    const raylib = raylib_build.addRaylib(b, target, optimise, .{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "nbody2",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
-        .optimize = optimise,
+        .optimize = optimize,
     });
+
+    const raylib_build = @import("raylib_build.zig");
+    const raylib = raylib_build.addRaylib(b, target, .ReleaseFast, .{});
     exe.linkLibrary(raylib);
     exe.addIncludePath(.{ .path = "raylib/src" });
+
+    switch (builtin.os.tag) {
+        inline .linux => {
+            exe.linkSystemLibrary("X11");
+            exe.linkSystemLibrary("xkbcommon");
+        },
+        inline else => {},
+        // inline else => |tag| @compileError(@tagName(tag) ++ " OS unsupported"),
+    }
 
     b.installArtifact(exe);
 }
