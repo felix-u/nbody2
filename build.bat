79f5bc2df49d64a06802a7ed24b55f13e5a046eb
Felix <29074327+felix-u@users.noreply.github.com>
2024-04-17 21:21:33 -0400

    begin work on c rewrite

--- build.bat
+++ build.bat
@@ -1,3 +1,13 @@
 @echo off
 
-zig build
+set deps=.\deps
+set dir_rl=%deps%\raylib
+
+set libs=%dir_rl%\lib\libraylib.a -I%dir_rl%\include -lgdi32 -lwinmm
+
+set cflags=-std=c99 -pedantic ^
+    -Wall -Werror -Wextra -Wshadow -Wconversion -Wdouble-promotion ^
+    -Wno-unused-function -Wno-sign-conversion -fno-strict-aliasing ^
+    -g3 -fsanitize=undefined -fsanitize-trap -DDEBUG
+
+zig cc %cflags% -o nbody2.exe src\main.c %libs%
